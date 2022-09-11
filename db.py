f99e57a001693a4c27639c869221b0d227e5a8fa
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-09-11 20:25:26 +0300

    Refactoring to modules

---
+++ db.py
@@ -0,0 +1,9 @@
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+uri = getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
+db = SQLAlchemy(app)
